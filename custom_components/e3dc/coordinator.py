b44f4025d1b6f8fc26ed5751e9da743221aa9e86
Simon Berger <simon@siku2.io>
2025-06-29 16:50:57 +0000

    Add title to hub

--- custom_components/e3dc/coordinator.py
+++ custom_components/e3dc/coordinator.py
@@ -135,7 +135,7 @@ class E3dcEntity[DescT: EntityDescription](CoordinatorEntity[E3dcCoordinator]):
             manufacturer=coordinator.common.manufacturer,
             model=coordinator.common.options if device_key is None else None,
             connections=connections,
-            translation_key=device_key,
+            translation_key="hub" if device_key is None else None,
             translation_placeholders={
                 "title": coordinator.config_entry.title,
             },
